c645321a77eee9809fa35d51e2e0af77ee3a6864
Ethan Malecot <ethanmalecot.99@gmail.com>
2019-12-06 07:21:48 +0100

    final1

--- View/api.js
+++ View/api.js
@@ -32,7 +32,7 @@ function connexion() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      if(xhttp.responseText.Message.equals("Connexion réussie.")){
+      if(JSON.parse(xhttp.responseText).Message.equals("Connexion réussie.")){
         document.cookie="motDePasse=" + "Vicor";
       } else {}
     }
