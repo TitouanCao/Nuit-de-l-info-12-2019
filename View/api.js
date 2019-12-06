bd21bd7b1e756729404755023485524089986ae5
Gilles Mertens <gilles.mertens@yahoo.fr>
2019-12-06 06:00:12 +0100

    qsdjhsq

--- View/api.js
+++ View/api.js
@@ -8,8 +8,8 @@ function inscrire() {
 
   xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    console.log(xhttp.responseText);
   }
+  console.log(xhttp.responseText);
   };
 
 
