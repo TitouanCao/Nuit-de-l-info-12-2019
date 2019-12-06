dfe59ea1dcde595526f38b452e126300aa022219
Ethan Malecot <ethanmalecot.99@gmail.com>
2019-12-06 07:16:54 +0100

    final1

--- View/api.js
+++ View/api.js
@@ -32,7 +32,7 @@ function connexion() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      if(xhhtp.responseText.Message=="Connexion réussie."){
+      if(xhttp.responseText.Message=="Connexion réussie."){
         document.cookie="motDePasse=" + "Victor";
       } else {}
     }
